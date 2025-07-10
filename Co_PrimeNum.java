edfda62720ac33b74815ef611e9dca37ab30c182
Rushi-Waghmare <rushikeshwaghmare53@gmail.com>
2025-07-10 17:33:41 +0530

    Add files via upload

---
+++ Co_PrimeNum.java
@@ -0,0 +1,15 @@
+//WAP to check no. is co-prime or not.
+//two primes(x,y) ----> divide it until y=0 (x%y==1)----> it is co-prime.
+import java.util.*;
+class Co_PrimeNum{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+    
+
+       while (true) { 
+           
+       }
+    }
+}
